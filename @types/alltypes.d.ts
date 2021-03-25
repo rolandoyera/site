feee28f901a5bccf4d9173be4235ff4a652eaa87
Rolando Yera <rolysemail@gmail.com>
2021-03-25 19:09:55 -0400

    Update alltypes.d.ts

--- @types/alltypes.d.ts
+++ @types/alltypes.d.ts
@@ -1,5 +1,6 @@
 declare module "rc-drawer"
 declare module "react-custom-scrollbars"
+declare module "@mdx-js/react"
 declare module "react-stickynode"
 declare module "react-scroll-up"
 declare module "gatsby-plugin-mailchimp"
