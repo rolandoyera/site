48a8fd4ac3dc23e1129b7301bfad63d5c07b734a
Rolando Yera <rolysemail@gmail.com>
2021-03-25 16:22:28 -0400

    types added to type file

--- src/components/card.js
+++ src/components/card.js
@@ -1,26 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledCard = styled.div`
+background-color: hsl(0, 0%, 68%, .1);
+padding: 20px;
+box-shadow: 0px 0px 3px 2px rgba(0,0,0,0.1);
+`;
 
 const Card = ({ children }) => {
   return (
-    <div>
-      <div style={{
-        backgroundColor: `hsl(0, 0%, 68%, .1)`,
-        paddingLeft: `20px`,
-        paddingTop: `10px`,
-        paddingBottom: `20px`,
-        paddingRight: `20px`,
-        borderRadius: `5px`,
-        border: `1px`,
-        borderStyle: `solid`,
-        borderColor: `hsl(0, 0%, 68%, .1)`,
-        boxShadow: `0 0.125rem 0.25rem rgba(0, 0, 0, 0.775) !important`,
-      }
-      }>
-        {children}
-      </div>
-
-
-    </div>
+    <StyledCard>
+      {children}
+    </StyledCard>
   );
 }
 
